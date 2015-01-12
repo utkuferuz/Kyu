e457f6f20d253ce57b5dd20dff8e21b73ee6f977
E. Serhan Boga <serhanbg@gmail.com>
2015-01-12 10:47:56 +0200

    d

--- Kyu/Controllers/DemoController.cs
+++ Kyu/Controllers/DemoController.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Kyu.Controllers
 {
+
+
     public class DemoController : Controller
     {
+        public DemoController()
+        {
+                
+        }
+        
         // GET: Demo
         public ActionResult Index()
         {
+
+
             return View();
         }
     }
