68790670551de6e2a5d07b62d7cfc67281fc7842
utkuferuz <utkuferuz@gmail.com>
2015-01-12 10:34:21 +0200

    Added new projects : Kyu.BusinessLogicLayer, Kyu.Logger Renamed Project : Kyu to Kyu.UI

---
+++ Kyu.BusinessLogicLayer/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kyu.BusinessLogicLayer
+{
+    public class Class1
+    {
+    }
+}
